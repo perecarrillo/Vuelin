895be88ca30653d25cfb35362bdf749bd58c0eec
marcordonez <marc.ordonez@estudiantat.upc.edu>
2023-05-13 13:37:16 +0200

    Canvas refacherito icons

--- canvas.py
+++ canvas.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter.colorchooser import askcolor
+from PIL import Image, ImageTk
+import customtkinter
 
 
 class Paint(object):
@@ -8,25 +10,29 @@ class Paint(object):
     DEFAULT_COLOR = 'black'
 
     def __init__(self):
-        self.root = Tk()
+        self.root = customtkinter.CTk()
 
-        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        #imgBrush = PhotoImage(file="./images/brush.png", size=(26, 26))
+        imgBrush = customtkinter.CTkImage(Image.open("./images/brush.png"), size=(30, 30))
+        imgEraser = customtkinter.CTkImage(Image.open("./images/eraser.png"), size=(30, 30))
+        imgPalette = customtkinter.CTkImage(Image.open("./images/palette.png"), size=(30, 30))
+        #imgPalette = PhotoImage(file="./images/palette.png")
+        #imgEraser = PhotoImage(file="./images/eraser.png")
 
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+        self.pen_button = customtkinter.CTkButton(self.root, text='', image=imgBrush, command=self.use_pen, width=80, hover_color='#FFB200', fg_color='#FFCB42', text_color='black')
+        self.pen_button.grid(row=0, column=0, padx=10, pady=10)
 
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
+        self.color_button = customtkinter.CTkButton(self.root, text='', image=imgPalette, command=self.choose_color, width=80, hover_color='#FFB200', fg_color='#FFCB42', text_color='black')
+        self.color_button.grid(row=0, column=1, padx=10, pady=10)
 
-        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
+        self.eraser_button = customtkinter.CTkButton(self.root, text='', image=imgEraser, command=self.use_eraser, width=80, hover_color='#FFB200', fg_color='#FFCB42', text_color='black')
+        self.eraser_button.grid(row=0, column=2, padx=10, pady=10)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+        self.choose_size_button = customtkinter.CTkSlider(self.root, from_=1, to=10, number_of_steps=10, width=130, fg_color='#ffff99', progress_color='#FFCB42', button_color='#FFCB42', button_hover_color='#FFB200')
+        self.choose_size_button.grid(row=0, column=3, padx=10, pady=10)
 
         self.c = Canvas(self.root, bg='white', width=600, height=600)
-        self.c.grid(row=1, columnspan=5)
+        self.c.grid(row=1, columnspan=4)
 
         self.setup()
         self.root.mainloop()
@@ -55,13 +61,13 @@ class Paint(object):
         self.activate_button(self.eraser_button, eraser_mode=True)
 
     def activate_button(self, some_button, eraser_mode=False):
-        self.active_button.config(relief=RAISED)
-        some_button.config(relief=SUNKEN)
+        # self.active_button.config(relief=RAISED)
+        # some_button.config(relief=SUNKEN)
         self.active_button = some_button
         self.eraser_on = eraser_mode
 
     def paint(self, event):
-        self.line_width = self.choose_size_button.get()
+        self.line_width = 7*self.choose_size_button.get() if self.eraser_on else self.choose_size_button.get()
         paint_color = 'white' if self.eraser_on else self.color
         if self.old_x and self.old_y:
             self.c.create_line(self.old_x, self.old_y, event.x, event.y,
