61c90c7361c072b4fa7d6aad1885ba898e53665f
perecarrillo <pere.carrillo.barrera@estudiantat.upc.edu>
2023-05-13 09:05:26 +0200

    Add bluetooth chat (test)

---
+++ host.py
@@ -0,0 +1,21 @@
+import socket as sc
+
+server = sc.socket(sc.AF_BLUETOOTH, sc.SOCK_STREAM, sc.BTPROTO_RFCOMM)
+server.bind(("c8:b2:9b:1a:74:b1", 4))
+server.listen(1)
+
+client, addr = server.accept()
+
+try:
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message: {data.decode('utf.8')}")
+        message = input("Enter message:")
+        client.send(message.encode("utf-8"))
+except OSError as e:
+    pass
+
+client.close()
+server.close()
