78d5b8f6c8aad271ae9047968e159adff7882215
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2023-05-14 01:26:49 +0200

    neteja codi i esquelet interfície

--- vuelin/polls/views.py
+++ vuelin/polls/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from django import forms
 
 from time import sleep 
-import player as pl
+import playerController as pl
 
 
 # playerloco = pl.Player()
@@ -52,7 +52,7 @@ def waitingRoomHost(request):
     return render(request,'waitingRoomHost.html',context)
 
 def waitingRoomPlayer(request):
-    sleep(3)
+    sleep(1)
     if gameIsReady():
         return redirect('writePrompt')
     else:
@@ -62,16 +62,34 @@ class PromptForm(forms.Form):
     prompt = forms.CharField(max_length=50)
 
 def writePrompt(request):
-    word = getPromptWord()
+    word = getPromptWord().upper()
     if request.method == 'POST':
         form = PromptForm(request.POST)
         if form.is_valid():
             prompt = form.cleaned_data['prompt']
             print(prompt)
-            if word in prompt.lower():
-                return redirect('drawImage')
+            if word in prompt.upper():
+                return redirect('canvas')
             else:
                 return render(request,'writePrompt.html',{"promptWord": word,'form': form})
     else:
         form = PromptForm()
     return render(request,'writePrompt.html',{"promptWord": word,'form': form})
+
+class PlayerNameForm(forms.Form):
+    name = forms.CharField(max_length=50)
+
+def playerNameInput(request):
+    word = getPromptWord()
+    if request.method == 'POST':
+        form = PlayerNameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            print(name)
+            return redirect('waitingRoomPlayer')
+    else:
+        form = PlayerNameForm()
+    return render(request,'playerNameInput.html',{'form': form})
+
+def canvas(request):
+    return render(request,"canvas.html")
