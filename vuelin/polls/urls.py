4256f7937cbe8ccdf5a42d9f9900885f391ac6db
LauraHPG <laura.hui.perez@estudiantat.upc.edu>
2023-05-13 17:08:55 +0200

    Proves varies

--- vuelin/polls/urls.py
+++ vuelin/polls/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls
     path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("gameCreator/", views.gameCreator, name="gameCreator"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("waitingRoomHost/", views.waitingRoomHost, name="waitingRoomHost"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
