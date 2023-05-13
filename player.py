04178f39a39d43483b78f564049477981ce09016
perecarrillo <pere.carrillo.barrera@estudiantat.upc.edu>
2023-05-13 16:24:33 +0200

    test

--- player.py
+++ player.py
@@ -4,11 +4,13 @@ client = None
 
 
 def connectToHost():
+    print("ConnectToHost")
     client = sc.socket(sc.AF_BLUETOOTH, sc.SOCK_STREAM, sc.BTPROTO_RFCOMM)
     client.connect(("c8:b2:9b:1a:74:b1", 4))
 
 
 def enterGame(name):
+    print("EnterGame " + str(name))
     client.send(name.encode("utf-8"))
 
 
